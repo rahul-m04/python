ff9c0980cffdec2feeef8cb1ee2dd9adc3b80b77
rahul-m04 <rahulmuthukrishnan09@gmail.com>
2026-07-28 23:03:58 +0530

    Add files via upload

---
+++ if else/tasks/task11.py
@@ -0,0 +1,11 @@
+sub1=int(input("mark1="))
+sub2=int(input("mark2="))
+sub3=int(input("mark3="))
+sub4=int(input("mark4="))
+sub5=int(input("mark5="))
+a=sub1+sub2+sub3+sub4+sub5
+b=a/5
+if(b<=35):
+    print("Additional class required")
+else:
+    print("you are free to go")
