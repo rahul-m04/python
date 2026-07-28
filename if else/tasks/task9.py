ccd116a2c99e171c35447b61660e7932a0dab035
rahul-m04 <rahulmuthukrishnan09@gmail.com>
2026-07-28 23:01:18 +0530

    Add files via upload

---
+++ if else/tasks/task9.py
@@ -0,0 +1,8 @@
+score=int(input("score percentage="))
+if(score>70):
+    name=input("Enter your name=")
+    dept=input("Enter your dept")
+    location=input("your location")
+    print("you are eligible")
+else:
+    print("you are not eligible")
